485da5c29ad7638b23fe7947188906514198b144
AndreiOiegar <andrei.oiegar@gmail.com>
2020-04-03 17:11:47 +0300

    Structured Games App

--- model.js
+++ model.js
@@ -1,66 +1,64 @@
 var apiURL = "https://games-world.herokuapp.com";
 
-fetch(apiURL + "/games", {
-    method: "GET",
-    headers: {
-        "Content-Type": "application/x-www-form-urlencoded"
-    }
-}).then(function(response){
-    return response.json();
-}).then(function(arrayOfGames){
-    //console.log("te response  ", arrayOfGames);
-
-    var container = document.querySelector('.container');
-
-    
-
-    // for(var i = 0; i < arrayOfGames.length; i++) {
-    //     //console.log(arrayOfGames[i]);
-    //     const h1 = document.createElement("h1");
-    //     const p = document.createElement("p");
-    //     const img = document.createElement("img");
+function getGamesList(callbackFunction){
+    fetch(apiURL + "/games", {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+        }
+    }).then(function(response){
+        return response.json();
+    }).then(function(arrayOfGames){
+        callbackFunction(arrayOfGames);
+    });
+}
 
 
-    //     h1.innerHTML = arrayOfGames[i].title;
-    //     p.innerHTML = arrayOfGames[i].description;
-
-    //     img.setAttribute("src", arrayOfGames[i].imageUrl);
-
-    //     // arrayOfGames[i].title
-    //     // arrayOfGames[i].description
-    //     // arrayOfGames[i].imageUrl
-
-    //     container.appendChild(h1);
-    //     container.appendChild(img);
-    //     container.appendChild(p);
-
-
-
-    // }
-
-    let gameElements = "";
-
-    for(var i = 0; i < arrayOfGames.length; i++) {
-        gameElements += "<h1>" + arrayOfGames[i].title + "</h1>" + 
-                            "<img src=" + arrayOfGames[i].imageUrl + " />" + 
-                            "<p> " + arrayOfGames[i].description + "</p>" + 
-                            "<button class='delete-btn' id='" + arrayOfGames[i]._id + "' onclick=\"deleteGame('" + arrayOfGames[i]._id + "')\">Delete</button>";
-        
-    }
-    container.innerHTML = gameElements;
-
-
-});
-
-function deleteGame(gameId){
-    //console.log("delete the game", gameId);
-
-    fetch(apiURL + "/games/" + gameId, {
+function deleteGame(gameID, callbackFunction) {
+    fetch(apiURL + "/games/" + gameID, {
         method: "DELETE"
     }).then(function(r){
         return r.text();
     }).then(function(apiresponse){
-        console.log(apiresponse);
-    })
+        callbackFunction(apiresponse);
+    });
+
 }
-var deleteBtns = document.getElementsByClassName(".delete-btn").addEventListener
+
+function createGameRequest(gameObject, callbackCreateGame){
+    fetch(apiURL + "/games", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        body: gameObject
+    }).then(function(response){
+        return response.json();
+    }).then(function(createdGame){
+        console.log(createdGame);
+        callbackCreateGame(createdGame);
+    });
+}
+
+
+function updateGameRequest(updatedGameObj, callbackCreateGame){
+    fetch(apiURL + "/games", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        body: gameObject
+    }).then(function(response){
+        return response.json();
+    }).then(function(updatedGame){
+        console.log(updatedGame);
+        callbackCreateGame(updatedGame);
+    });
+}
+
+
+// "application/json"
+// {"cheie": "valoare", "cheie2": "valoare2"}
+
+//application/x-www-form-urlencoded
+// cheie=valoare&cheie2=valoare2
